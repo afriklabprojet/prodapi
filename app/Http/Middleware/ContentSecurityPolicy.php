1447b4a158b90c71639a5e03b3db999d920254c9
afriklabprojet <afriklabprojet@gmail.com>
2026-04-15 16:35:39 +0200

    fix(csp): add unpkg.com + cdn.jsdelivr.net to script-src and style-src for Leaflet

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -14,8 +14,8 @@ class ContentSecurityPolicy
 
         $response->headers->set('Content-Security-Policy', implode('; ', [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net",
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://unpkg.com https://cdn.jsdelivr.net",
             "font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net",
             "img-src 'self' data: blob: https://*.tile.openstreetmap.org https://ui-avatars.com",
             "connect-src 'self'",
